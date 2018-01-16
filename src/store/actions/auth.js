3662143e1b4094df43c26355343afb6c8461728d
Susan Wang <susanw@dev6.com>
2018-01-16 13:53:26 -0500

    AsyncStorage to save token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import { AsyncStorage } from 'react-native';
 import { TRY_AUTH, AUTH_SET_TOKEN } from "./actionTypes";
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
@@ -35,10 +36,18 @@ export const tryAuth = (authData, authMode) => {
           if (!parsedRes.idToken) {
             alert("Authentication failed, please try again!");
           } else {
-            dispatch(authSetToken(parsedRes.idToken));
+            dispatch(authStoreToken(parsedRes.idToken));
             startMainTabs();
           }
         });
+  };
+};
+
+export const authStoreToken = token => {
+  return dispatch => {
+    dispatch(authSetToken(token));
+    // mfa for my-first-app
+    AsyncStorage.setItem("mfa:auth:token", token);
   };
 };
 
@@ -54,7 +63,12 @@ export const authGetToken = () => {
     const promise = new Promise((resolve, reject) => {
       const token = getState().auth.token;
       if (!token) {
-        reject();
+        AsyncStorage.getItem("mfa:auth:token")
+          .catch(err => reject())
+          .then(tokenFromStorage => {
+            dispatch(authSetToken(tokenFromStorage));
+            resolve(tokenFromStorage);
+          });
       } else {
         resolve(token);
       }
