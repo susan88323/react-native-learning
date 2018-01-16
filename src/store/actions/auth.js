15fc9131f63bec2f4c0f3422b4bf69c6d1dfb6f6
Susan Wang <susanw@dev6.com>
2018-01-16 13:41:34 -0500

    Add auth token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,4 @@
-import { TRY_AUTH } from "./actionTypes";
+import { TRY_AUTH, AUTH_SET_TOKEN } from "./actionTypes";
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
@@ -32,11 +32,33 @@ export const tryAuth = (authData, authMode) => {
         .then(res => res.json())
         .then(parsedRes => {
           dispatch(uiStopLoading());
-          if (parsedRes.error) {
+          if (!parsedRes.idToken) {
             alert("Authentication failed, please try again!");
           } else {
+            dispatch(authSetToken(parsedRes.idToken));
             startMainTabs();
           }
         });
+  };
+};
+
+export const authSetToken = token => {
+  return {
+    type: AUTH_SET_TOKEN,
+    token: token
+  };
+};
+
+export const authGetToken = () => {
+  return (dispatch, getState) => {
+    const promise = new Promise((resolve, reject) => {
+      const token = getState().auth.token;
+      if (!token) {
+        reject();
+      } else {
+        resolve(token);
+      }
+    });
+    return promise;
   };
 };
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,64 +1,86 @@
 import { SET_PLACES, REMOVE_PLACE } from './actionTypes';
-import { uiStartLoading, uiStopLoading } from './index';
+import { uiStartLoading, uiStopLoading, authGetToken } from './index';
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
+    let authToken;
     dispatch(uiStartLoading());
-    fetch("https://us-central1-my-first-app-1515781516874.cloudfunctions.net/storeImage", {
-      method: "POST",
-      body: JSON.stringify({
-        image: image.base64
+    dispatch(authGetToken())
+      .catch(() => {
+        alert("No valid token found!");
       })
-    })
-    .catch(err => {
-      console.log(err);
-      dispatch(uiStopLoading());
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      const placeData = {
-        name: placeName,
-        location: location,
-        image: parsedRes.imageUrl
-      };
-      return fetch("https://my-first-app-1515781516874.firebaseio.com/places.json", {
-        method: "POST",
-        body: JSON.stringify(placeData)
+      .then(token => {
+        authToken = token;
+        return fetch("https://us-central1-my-first-app-1515781516874.cloudfunctions.net/storeImage", {
+          method: "POST",
+          body: JSON.stringify({
+            image: image.base64
+          }),
+          headers: {
+            "Authorization": "Bearer " + authToken
+          }
+        })
       })
-    })
-    .catch(err => {
-      console.log(err);
-      dispatch(uiStopLoading());
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      console.log(parsedRes);
-      dispatch(uiStopLoading());
-    });
+      .catch(err => {
+        console.log(err);
+        alert("Something went wrong, please try again!");
+        dispatch(uiStopLoading());
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedRes.imageUrl
+        };
+        return fetch("https://my-first-app-1515781516874.firebaseio.com/places.json?auth=" + authToken, {
+          method: "POST",
+          body: JSON.stringify(placeData)
+        })
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+        dispatch(uiStopLoading());
+      })
+      .catch(err => {
+        console.log(err);
+        alert("Something went wrong, please try again!");
+        dispatch(uiStopLoading());
+      });
   };
 };
 
 export const getPlaces = () => {
   return dispatch => {
-    fetch("https://my-first-app-1515781516874.firebaseio.com/places.json")
-    .catch(err => {
-      alert("Something went wrong");
-      console.log(err);
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      const places = [];
-      for (let key in parsedRes) {
-        places.push({
-          ...parsedRes[key],
-          image: {
-            uri: parsedRes[key].image
-          },
-          key: key
-        });
-      }
-      dispatch(setPlaces(places));
-    });
+    dispatch(authGetToken())
+      .then(token => {
+        return fetch(
+          "https://my-first-app-1515781516874.firebaseio.com/places.json?auth=" +
+            token
+        );
+      })
+      .catch(() => {
+        alert("No valid token found!");
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        const places = [];
+        for (let key in parsedRes) {
+          places.push({
+            ...parsedRes[key],
+            image: {
+              uri: parsedRes[key].image
+            },
+            key: key
+          });
+        }
+        dispatch(setPlaces(places));
+      })
+      .catch(err => {
+        alert("Something went wrong, sorry :/");
+        console.log(err);
+      });
   };
 };
 
@@ -71,18 +93,24 @@ export const setPlaces = places => {
 
 export const deletePlace = (key) => {
   return dispatch => {
-    dispatch(removePlace(key));
-    fetch("https://my-first-app-1515781516874.firebaseio.com/places/" + key + ".json", {
-      method: "DELETE"
-    })
-    .catch(err => {
-      alert("Something went wrong");
-      console.log(err);
-    })
-    .then(res => res.json())
-    .then(parsedRes => {
-      console.log("Done!");
-    });
+    dispatch(authGetToken())
+      .catch(() => {
+        alert("No valid token found!");
+      })
+      .then(token => {
+        dispatch(removePlace(key));
+        return fetch("https://my-first-app-1515781516874.firebaseio.com/places/" + key + ".json?auth=" + token, {
+          method: "DELETE"
+        })
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log("Done!");
+      })
+      .catch(err => {
+        alert("Something went wrong");
+        console.log(err);
+      });
   };
 };
 
