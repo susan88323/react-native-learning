bcbef6171f76b65c8f1905a888fc364564f7ae03
Susan Wang <susanw@dev6.com>
2018-01-17 13:45:34 -0500

    finalize

--- android/app/src/main/java/com/myfirstapp/MainActivity.java
+++ android/app/src/main/java/com/myfirstapp/MainActivity.java
@@ -1,6 +1,27 @@
 package com.myfirstapp;
 
 import com.facebook.react.ReactActivity;
+import android.widget.LinearLayout;
+import android.graphics.Color;
+import android.widget.TextView;
+import android.view.Gravity;
+import android.util.TypedValue;
 
 public class MainActivity extends com.reactnativenavigation.controllers.SplashActivity {
+  @Override
+  public LinearLayout createSplashLayout() {
+    LinearLayout view = new LinearLayout(this);
+    TextView textView = new TextView(this);
+
+    view.setBackgroundColor(Color.parseColor("#521751"));
+    view.setGravity(Gravity.CENTER);
+
+    textView.setTextColor(Color.parseColor("#fa923f"));
+    textView.setText("Awesome Places");
+    textView.setGravity(Gravity.CENTER);
+    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
+
+    view.addView(textView);
+    return view;
+  }
 }
